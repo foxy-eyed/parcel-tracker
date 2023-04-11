19b7a5f64a52686994bfad7edc3e2d3947d8cda3
Anna Selivanova <foxy.eyed@gmail.com>
2023-04-11 20:26:09 +0300

    Get subscription info

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
 class SubscriptionsController < ApplicationController
-  def show; end
+  def index
+    subscription = package_resource.subscriptions.find_by(subscriber: current_user)
+    render json: subscription, only: %i[email phone enabled]
+  end
 
   def create
     subscription = package_resource.subscriptions.find_or_initialize_by(subscriber: current_user)
-    p subscription_params
     result = SubscriptionContract.new.call(**subscription_params)
     if result.success?
       subscription.update!(result.to_h)
-      render json: subscription
+      render json: subscription, only: %i[email phone enabled]
     else
       render status: :unprocessable_entity, json: { errors: result.errors.to_h }
     end
@@ -22,7 +24,6 @@ class SubscriptionsController < ApplicationController
   end
 
   def subscription_params
-    # params.permit(:package_id, :email, :phone, enabled: [])
-    params.permit!
+    params.permit(:package_id, :email, :phone, enabled: [])
   end
 end
