c090078b3da289174e2e3940c365fa98b6062901
Anna Selivanova <foxy.eyed@gmail.com>
2023-04-12 08:44:29 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user = User.create(id: "defb5761-02b8-45ed-b660-8092a4c8d884", name: "Anna")
+package_hp = Package.create(id: "5ee90d5a-d313-410d-a1b4-36d383b02ab6", track: "HP0001")
+package_fd = Package.create(id: "1a133134-28ce-4dfa-8bca-7d84fdc5995e", track: "FD0001")
+
+user.subscriptions.create(
+  [
+    { package: package_hp, email: "anna@mail.test", enabled: ["email"] },
+    { package: package_fd, email: "anna@mail.test", phone: "+357 12349009", enabled: %w[phone email] }
+  ]
+)
